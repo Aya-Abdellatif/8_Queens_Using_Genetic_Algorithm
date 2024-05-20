59e53dc0be653be2d87040e772a147ed46d5e039
Aya Abdellatif <ayaabdllatif112@gmail.com>
2024-05-20 19:28:19 +0300

    errors have been fixed"

--- Source_Code/8_queens_using_genetic_algorithm.py
+++ Source_Code/8_queens_using_genetic_algorithm.py
@@ -7,28 +7,26 @@ POPULATION_SIZE = 100
 MUTATION_RATE = 0.1 
 MAX_FITNESS = 28  #NUM_QUEENS *(NUM_QUEENS-1)/2
 
-
-def CreateBoard():
+#Creates a random board
+def create_board():
     return [random.randint(0, NUM_QUEENS - 1) for _ in range(NUM_QUEENS)]
 
 
-#Fitness function
-def Fitness(board):
+#The number of non-attacking pairs of queens on a board
+def fitness(board):
     clashes = 0 
     for i in range(NUM_QUEENS):
-        r = board[i]
-        for j in range(NUM_QUEENS):
-            if i != j:
-                d = abs(i - j)
-                if board[j] in [r, r - d, r + d]:
-                    clashes += 1
+        for j in range(i + 1, NUM_QUEENS):
+            if board[i] == board[j] or abs(board[i] - board[j]) == abs(i - j):
+                clashes += 1
     return MAX_FITNESS - clashes #28 - clashes
 
-#wheel_selection
-def Selection(population):
+
+#Roulette wheel selection
+def selection(population):
     selected_population = [] 
 
-    fitness_values = [Fitness(board) for board in population]
+    fitness_values = [fitness(board) for board in population]
     total_fitness = sum(fitness_values)
 
     normalized_fitness = [each / total_fitness for each in fitness_values]
@@ -44,85 +42,98 @@ def Selection(population):
 
     return selected_population
 
+
+#Crossover between two parents(boards) to create two children.
 def Crossover(parent1, parent2):
-    crossover_point = random.randint(1, NUM_QUEENS - 1) #RANDOM POINT
+    crossover_point = random.randint(1, NUM_QUEENS - 1) #Random Point
     child1 = parent1[:crossover_point] + parent2[crossover_point:]
     child2 = parent2[:crossover_point] + parent1[crossover_point:]
 
     return child1, child2
 
 
-def Mutation(board):
+#Mutates a board with a certain probability.
+def mutation(board):
     if random.random() < MUTATION_RATE:
-        gene_to_mutate = random.randint(0, NUM_QUEENS - 1)
+        idx = random.randint(0, NUM_QUEENS - 1)
         new_value = random.randint(0, NUM_QUEENS - 1)
-        while new_value == board[gene_to_mutate]:
+        while new_value == board[idx]:
             new_value = random.randint(0, NUM_QUEENS - 1)
-        board[gene_to_mutate] = new_value
+        board[idx] = new_value
 
-        return board
+    return board
     
 
-def PrintChessBoard(board):
-  
-  for row in range(NUM_QUEENS -1, -1, -1):
-    for col in range(NUM_QUEENS):
-        if board[col] == row:
-            print("Q", end=" ")
-        else:
-            print("-", end=" ")  
+#Prints the board with queens represented by 'Q'.
+def print_chess_board(board):
+    for row in range(NUM_QUEENS + 1):
+        print(row - 1 if row != 0 else ' ', end=' ')#print x axis
     print()
+    for row in range(NUM_QUEENS - 1, -1, -1):
+        print(row, end=' ')#print y axis
+        for col in range(NUM_QUEENS):
+            print('Q' if board[col] == row else '-', end=' ')
+        print()
 
-#Genetic algorith
-def GeneticAlgorithm(num_of_generations):
-    population = [CreateBoard() for _ in range(POPULATION_SIZE)] 
+
+#Prints the solution found by the genetic algorithm.
+def print_solution(best_fitness, best_board, generation):
+    print("\n" + "="*51)
+    if best_fitness != MAX_FITNESS:
+        print("No optimal solution found.")
+    else:
+        print(f"Optimal solution found in {generation + 1} generations:")
+        print(f"Board: {best_board}")
+        print(f"Fitness: {best_fitness}")
+        print("="*51)
+        print_chess_board(best_board)
+
+
+#The genetic algorithm to solve the N-Queens problem."""
+def genetic_algorithm(num_generations):
+    population = [create_board() for _ in range(POPULATION_SIZE)] 
     best_fitness = 0
     best_board = None
-    for generation in range(num_of_generations):
+
+    for generation in range(num_generations):
         #selection
-        selected = Selection(population)
+        selected_population = selection(population)
 
         #crossover
         children = []
         for i in range(0, POPULATION_SIZE, 2):
-            parent1 = selected[i]
-            parent2 = selected[i+1]
+            parent1 = selected_population[i]
+            parent2 = selected_population[i+1]
             child1, child2 = Crossover(parent1, parent2)
             children.append(child1)
             children.append(child2)
             
-
         #Mutation
         for child in children:
-            Mutation(child)
+            mutation(child)
 
-
-        population = children # new population
+        # new population
+        population = children 
 
         #update best solution
-        current_fitness = max(Fitness(board) for board in population)
+        current_fitness = max(fitness(board) for board in population)
+
         if current_fitness > best_fitness:
             best_fitness = current_fitness
-            best_board = max(population, key = Fitness)
+            best_board = max(population, key = fitness)
 
         #print progress
         print(f'\rGeneration: {generation + 1}, best fitness: {best_fitness}', end = '')
+
         if current_fitness == MAX_FITNESS: #28
             break
 
     #print solution
-    if(best_fitness != MAX_FITNESS):
-        print("\nNO OPTIMAL SOLUTION FOUND")
-    else:
-        print(f"\nOPTIMAL SOLUTION FOUND IN {generation + 1} GENERATIONS: ")
-        print(f"BOARD: {best_board}")
-        print(f"FITNESS: {best_fitness}") #28
-        print('===================================================')
-        PrintChessBoard(best_board)
+    print_solution(best_fitness, best_board, generation)
 
 
 if __name__ == "__main__":
-    print('===================================================')
-    num_of_generations = int(input("Enter the number of Generations: "))
-    print('===================================================')
-    GeneticAlgorithm(num_of_generations)
+    print("="*51)
+    num_generations = int(input("Enter the number of generations: "))
+    print("="*51)
+    genetic_algorithm(num_generations)
